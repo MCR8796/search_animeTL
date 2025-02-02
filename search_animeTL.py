99fa7dcb8b7074307f89b33cb607516c54260d28
MCR8796 <tou01221@gmail.com>
2025-02-03 00:44:15 +0900

    Update search_animeTL.py

--- search_animeTL.py
+++ search_animeTL.py
@@ -69,9 +69,6 @@ for i, week in enumerate(anime['broadcast']):
     # 放送開始日時から週番号を基に放送日時を計算
     broadcast_start = first_broadcast_start + timedelta(weeks=i)
     if week is not None:
-        if (i + 1) < week:
-            for j in range(week - i - 1):
-                broadcast.append(broadcast_start)
         # 現在日時より前の放送分を計算
         if broadcast_start <= current_datetime:
             episodes.append(week)
